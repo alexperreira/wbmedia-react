24194ffe6a0b113e17b12c4044322ae58c228713
Alex <alex@thealexperr.com>
2021-12-12 10:30:13 -0700

    added files in root directory, namely App.js and index.css

---
+++ src/components/Sections/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<React.Fragment>
+			<header>
+				<div className='container'>
+					<img src='' alt='' className='logo' />
+					<nav className='header-nav'>
+						<ul>
+							<li>
+								<a href=''>Packages</a>
+							</li>
+							<li>
+								<a href='faq.html'>FAQ</a>
+							</li>
+							<li>
+								<a href='https://www.honeybook.com/widget/w_bilbao_media_llc_170936/cf_id/5fda3e235ff8e50a0f01364c'>
+									Get in Touch!
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+		</React.Fragment>
+	);
+};
+
+export default Header;
