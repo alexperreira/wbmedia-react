124f51ff674b0d10a389a8359664593a8d69f233
Alex <alex@thealexperr.com>
2022-01-02 14:30:11 -0700

    added Router

--- src/components/Sections/Header.js
+++ src/components/Sections/Header.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './Header.module.css';
+import logo from '../../assets/WB-Submark-CMYK.png';
 
 const Header = () => {
 	return (
 		<React.Fragment>
 			<header>
-				<div className='container'>
-					<img src='' alt='' className='logo' />
-					<nav className='header-nav'>
-						<ul>
-							<li>
-								<a href=''>Packages</a>
-							</li>
-							<li>
-								<a href='faq.html'>FAQ</a>
-							</li>
-							<li>
-								<a href='https://www.honeybook.com/widget/w_bilbao_media_llc_170936/cf_id/5fda3e235ff8e50a0f01364c'>
-									Get in Touch!
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
+				<Router>
+					<div className='container'>
+						<img src={logo} alt='' className='logo' />
+						<nav className='header-nav'>
+							<Switch>
+								<Route path='/about'>
+									<About />
+								</Route>
+								<Route path='/packages'>
+									<Packages />
+								</Route>
+								<Route path='/'>
+									<Home />
+								</Route>
+							</Switch>
+							<ul>
+								<li>
+									<Link to='/packages'></Link>
+								</li>
+								<li>
+									<Link to='/faq'>FAQ</Link>
+								</li>
+								<li>
+									<a href='https://www.honeybook.com/widget/w_bilbao_media_llc_170936/cf_id/5fda3e235ff8e50a0f01364c'>
+										Get in Touch!
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</Router>
 			</header>
 		</React.Fragment>
 	);
