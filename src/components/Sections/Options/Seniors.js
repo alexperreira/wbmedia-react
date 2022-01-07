565898651e7cb04da7a9c984660f8846bf4929fc
Alex Perreira <alex@thealexperr.com>
2022-01-06 17:19:05 -0700

    end of work day

--- src/components/Sections/Options/Seniors.js
+++ src/components/Sections/Options/Seniors.js
@@ -1,10 +1,12 @@
 import React from 'react';
+// import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import styles from './Seniors.module.css';
 
 const Seniors = () => {
 	return (
 		<div className={styles.seniors}>
 			<h1>Seniors tab display</h1>
+			<p>l</p>
 		</div>
 	);
 };
