5f6f46785a2b7b84da1bf6b980d83023037f6278
Alex Perreira <alexanderperreira@gmail.com>
2022-01-09 11:11:54 -0700

    git testing version control

--- src/index.js
+++ src/index.js
@@ -4,21 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
-// import Home from './components/Pages/Home';
-// import Packages from './components/Pages/Packages';
-// import FAQ from './components/Pages/FAQ';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
-			{/* <Routes>
-			<Route path='/' element={<App />}>
-			<Route index element={<Home />} />
-			<Route path={Packages} element={<Packages />} />
-			<Route path='faq' element={<FAQ />} />
-			</Route>
-		</Routes> */}
 			<App />
 		</Router>
 	</React.StrictMode>,
