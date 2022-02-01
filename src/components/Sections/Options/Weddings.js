602600bd7925f13c0584235c3ed52114286d4039
Alex Perreira <alex@thealexperr.com>
2022-02-01 12:43:52 -0700

    added styling and content to Weddings page

--- src/components/Sections/Options/Weddings.js
+++ src/components/Sections/Options/Weddings.js
@@ -60,6 +60,59 @@ const Weddings = () => {
 					<Button text='Inquire here' />
 				</div>
 			</div>
+			<div className={styles.addonsContainer}>
+				<h3 className={styles.addonsTitle}>Add-Ons</h3>
+				<hr className={styles.thin} />
+				<h4 className={styles.addon}>Second Shooter</h4>
+				<div className={styles.addonContainer}>
+					<h5 className={styles.addonPrice}>$350</h5>
+					<p className={styles.addonText}>
+						I do require a second shooter for weddings that are bigger than 125
+						people. I primarily focus my time on the Bride + Groom + wedding
+						party and with more people than that, I don't get as many photos of
+						guests. You want to remember every one who celebrated your love,
+						&amp; my second shooter will help with that!
+					</p>
+				</div>
+				<hr className={styles.thin} />
+				<h4 className={styles.addon}>Polaroids</h4>
+				<div className={styles.addonContainer}>
+					<h5 className={styles.addonPrice}>$150</h5>
+					<p className={styles.addonText}>
+						Polaroids images that are JUST FOR YOU! of every part of the day.
+						<br />
+						<br />
+						Some people have them for guests, but these are just for my Bride +
+						Groom.
+					</p>
+				</div>
+				<hr className={styles.thin} />
+				<h4 className={styles.addon}>Boudoir</h4>
+				<div className={styles.addonContainer}>
+					<h5 className={styles.addonPrice}>$250</h5>
+					<ul>
+						<li className={styles.disclaimer}>
+							*Airbnb/hotel fee not included*
+						</li>
+						<li className={styles.addonText}>Discounted rate for my brides!</li>
+						<li className={styles.addonText}>
+							I do not have a space. But I can offer the best Airnbs or hotels
+							that I have worked at.{' '}
+						</li>
+						<li className={styles.disclaimer}>
+							(It can be added to a secret invoice.)
+						</li>
+					</ul>
+				</div>
+				<hr className={styles.thin} />
+				<div className={styles.addonContainer}>
+					<p className={styles.addonText}>
+						EVERY wedding is insanely different. These packages can be
+						customized to meet your needs. So please feel free to ask if you're
+						curious about changing something.
+					</p>
+				</div>
+			</div>
 			<Footer />
 		</div>
 	);
