8aa222ed15a21cdd58e3b1d09387150eb5d31046
Alex Perreira <alexanderperreira@gmail.com>
2022-03-12 10:01:54 -0700

    committing for cross platform work. Working on integrating instagram API

--- src/components/UI/Instagram/InstaFeed.js
+++ src/components/UI/Instagram/InstaFeed.js
@@ -1,6 +1,35 @@
 import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 
-const url = `/src/functions/instagram.js`;
+const url = `https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables=%7B%22id%22%3A%224476803220%22%2C%22first%22%3A12%2C%22after%22%3A%22QVFCd01XY1ZUX3c4Ym5ORUZuNmhILVBLVi1pVGJpeEdDR3ZNMEdJYVFIbXFYVXJSSXh0Tmhfd251X2hBWVBLc0x3ZkxjTlplSGxKR0NiU3o5clFUR1BCWQ%3D%3D%22%7D`;
+
+// const cache = {
+// 	lastFetch: 0,
+// 	posts: [],
+// };
+
+// async function getPosts() {
+// 	const timeSinceLastFetch = Date.now() - cache.lastFetch;
+// 	if (timeSinceLastFetch <= 1800000) {
+// 		return cache.posts;
+// 	}
+
+// 	const data = await fetch(url).then((res) => res.json());
+// 	const posts = slimUpPosts(data);
+// 	cache.lastFetch = Date.now();
+// 	cache.posts = posts;
+// 	console.log(data);
+// 	return data;
+// }
+
+// function slimUpPosts(response) {
+// 	return response.data.user.edge_owner_to_timeline_media.edges.map((edge) => ({
+// 		thumbnail: edge.node.thumbnail_src,
+// 		url: `https://instagram.com/p/${edge.node.shortcode}`,
+// 		caption: edge.node.edge_media_to_caption.edges[0].node.text,
+// 		id: edge.node.id,
+// 	}));
+// }
 
 function useInstagram() {
 	const [posts, setPosts] = useState([]);
@@ -8,21 +37,9 @@ function useInstagram() {
 		fetch(url)
 			.then((res) => res.json())
 			.then((data) => {
+				console.log(data);
 				setPosts(data);
 			});
-		// fetch(url)
-		// 	.then(async (response) => {
-		// 		try {
-		// 			const data = await response.json();
-		// 			console.log('response data', data);
-		// 		} catch (error) {
-		// 			console.log('Error happened here!');
-		// 			console.log(error);
-		// 		}
-		// 	})
-		// 	.then((data) => {
-		// 		setPosts(data);
-		// 	});
 	}, []);
 	return posts;
 }
--- src/functions/instagram.js
+++ src/functions/instagram.js
@@ -1,8 +1,8 @@
 import 'isomorphic-fetch';
 
-// const Url = `https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables=%7B%22id%22%3A%224476803220%22%2C%22first%22%3A12%2C%22after%22%3A%22QVFCd01XY1ZUX3c4Ym5ORUZuNmhILVBLVi1pVGJpeEdDR3ZNMEdJYVFIbXFYVXJSSXh0Tmhfd251X2hBWVBLc0x3ZkxjTlplSGxKR0NiU3o5clFUR1BCWQ%3D%3D%22%7D`;
+const url = `https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables=%7B%22id%22%3A%224476803220%22%2C%22first%22%3A12%2C%22after%22%3A%22QVFCd01XY1ZUX3c4Ym5ORUZuNmhILVBLVi1pVGJpeEdDR3ZNMEdJYVFIbXFYVXJSSXh0Tmhfd251X2hBWVBLc0x3ZkxjTlplSGxKR0NiU3o5clFUR1BCWQ%3D%3D%22%7D`;
 
-const Url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,permalink&access_token=IGQVJXSmlmWXhkQ2gwMC10c0VseW5BLVN5bjlCMXFOM3ZAMQXNPMlVNaHBRMzA5T3dLdXBMX2RLN2xOTUNvd3VQcFQ2SWU3amcwUWREdjJHWEtfVEc1TEZAqeTBZATU13cThOSnZAvajN6eUlIOG9lWkxfcHhrc01oQ0RUU2ZAR`;
+// const Url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,permalink&access_token=IGQVJXSmlmWXhkQ2gwMC10c0VseW5BLVN5bjlCMXFOM3ZAMQXNPMlVNaHBRMzA5T3dLdXBMX2RLN2xOTUNvd3VQcFQ2SWU3amcwUWREdjJHWEtfVEc1TEZAqeTBZATU13cThOSnZAvajN6eUlIOG9lWkxfcHhrc01oQ0RUU2ZAR`;
 
 const cache = {
 	lastFetch: 0,
@@ -15,7 +15,7 @@ async function getPosts() {
 	if (timeSinceLastFetch <= 1800000) {
 		return cache.posts;
 	}
-	const data = await fetch(Url).then((res) => res.json());
+	const data = await fetch(url).then((res) => res.json());
 	const posts = slimUpPosts(data);
 	cache.lastFetch = Date.now();
 	cache.posts = posts;
@@ -24,18 +24,18 @@ async function getPosts() {
 }
 
 function slimUpPosts(response) {
-	// return response.data.user.edge_owner_to_timeline_media.edges.map((edge) => ({
-	// 	thumbnail: edge.node.thumbnail_src,
-	// 	url: `https://instagram.com/p/${edge.node.shortcode}`,
-	// 	caption: edge.node.edge_media_to_caption?.edges[0].node.text,
-	// 	id: edge.node.id,
-	// }));
-	return response.data.map((dat) => ({
-		thumbnail: dat.media_url,
-		url: dat.permalink,
-		caption: dat.caption,
-		id: dat.id,
+	return response.data.user.edge_owner_to_timeline_media.edges.map((edge) => ({
+		thumbnail: edge.node.thumbnail_src,
+		url: `https://instagram.com/p/${edge.node.shortcode}`,
+		caption: edge.node.edge_media_to_caption.edges[0].node.text,
+		id: edge.node.id,
 	}));
+	// return response.data.map((dat) => ({
+	// 	thumbnail: dat.media_url,
+	// 	url: dat.permalink,
+	// 	caption: dat.caption,
+	// 	id: dat.id,
+	// }));
 }
 
 exports.handler = async function (event, context, callback) {
