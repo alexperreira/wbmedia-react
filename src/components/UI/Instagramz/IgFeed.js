ae8cbec3078560ede9b61ffd54f54b58054854d3
Alex Perreira <alexanderperreira@gmail.com>
2022-04-27 17:39:22 -0600

    changed styling and modified instagram feed

--- src/components/UI/Instagramz/IgFeed.js
+++ src/components/UI/Instagramz/IgFeed.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
 
 const IgFeed = () => {
 	return (
 		<div>
-			<link
-				href='https://assets.juicer.io/embed.css'
-				media='all'
-				rel='stylesheet'
-				type='text/css'
+			<InstagramEmbed
+				url='https://i.instagram.com/api/v1/feed/timeline/'
+				clientAccessToken='936619743392459|3cdb3f896252a1db29679cb4554db266'
 			/>
-			<ul class='juicer-feed' data-feed-id='bilbaowhitneyphoto'></ul>
 		</div>
 	);
 };
